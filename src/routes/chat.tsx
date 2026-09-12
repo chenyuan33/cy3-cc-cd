6d696fc1589507a0d71c0b431454db5e9a5b527f
guangzhoueven <10062132@qq.com>
2026-09-12 19:45:12 +0800

    Upd at 2026-09-12: 仅针对一个页面的 CSS/JS 单独开一个文件

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -76,6 +76,7 @@ app.get('/', async c => {
 
     return c.render(<Card style={{ position: 'fixed', top: '50px', bottom: '10px', left: '70px', right: '10px' }}>
         <MdInit />
+        <link rel='stylesheet' type='text/css' href='/chat.css' />
         <h1>{getText(locale, 'chat')}</h1>
         <div style={{ border: 'solid', 'border-radius': '10px', display: 'flex', position: 'absolute', top: '100px', bottom: '10px', left: '10px', right: '10px' }}>
             <div style={{ padding: '10px', 'border-right': 'solid 1px lightgray', position: 'relative', overflow: 'auto', width: '280px', flexShrink: 0 }}>
